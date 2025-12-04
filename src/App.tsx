53ff79ba6879e400b91c8189b8365cb13267d0e6
Yash Panchal <panchalyash089@gmail.com>
2025-12-04 10:53:25 +0530

    removed login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import RegisterKid from "./pages/RegisterKid";
 import KidsList from "./pages/KidsList";
 import MarkAttendance from "./pages/MarkAttendance";
 import AttendanceHistory from "./pages/AttendanceHistory";
 import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 
 const queryClient = new QueryClient();
 
@@ -21,58 +19,11 @@ const App = () => (
       <Sonner />
       <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/"
-            element={
-              localStorage.getItem("authUser") ? (
-                <Index />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              localStorage.getItem("authUser") ? (
-                <RegisterKid />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
-          <Route
-            path="/kids"
-            element={
-              localStorage.getItem("authUser") ? (
-                <KidsList />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
-          <Route
-            path="/attendance"
-            element={
-              localStorage.getItem("authUser") ? (
-                <MarkAttendance />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
-          <Route
-            path="/history"
-            element={
-              localStorage.getItem("authUser") ? (
-                <AttendanceHistory />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
+          <Route path="/" element={<Index />} />
+          <Route path="/register" element={<RegisterKid />} />
+          <Route path="/kids" element={<KidsList />} />
+          <Route path="/attendance" element={<MarkAttendance />} />
+          <Route path="/history" element={<AttendanceHistory />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
